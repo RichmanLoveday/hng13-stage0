1d89be601e0a753b1c706f8bdfdc7f0c22f12c18
RichmanLoveday <lovedayrichman@yahoo.com>
2025-10-16 17:13:48 +0100

    added a line to run migration, fix

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
 
 //? code to run migration since i am using PXXL App
 Route::get('/migrate-now', function () {
+    Artisan::call('session:table');
     Artisan::call('migrate', ['--force' => true]);
     return "Migration completed!";
 });
